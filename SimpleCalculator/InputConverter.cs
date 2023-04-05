93ce2e73d6771d7960e4420b106f7d447ee3768c
GeorgianaDumitru <georgianadumitru3200@gmail.com>
2023-04-05 09:08:26 +0300

    Initial realease

---
+++ SimpleCalculator/InputConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class InputConverter
+    {
+        public double ConvertInputToNumberic(string argTextInput)
+        {
+            double convertedNum;
+            if (!double.TryParse(argTextInput, out convertedNum)) throw new ArgumentException("Expected a numeric value.");
+            return convertedNum;
+        }
+    }
+}
---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                double firstNum = inputConverter.ConvertInputToNumberic(Console.ReadLine());
+                double secondNum = inputConverter.ConvertInputToNumberic(Console.ReadLine());
+                string operation = Console.ReadLine();
+
+                double result = calculatorEngine.Calculate(operation, firstNum, secondNum);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+
+    }
+}
